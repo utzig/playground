f0fdff02cb6347bbc62fb1faa519c1b79f56947a
Fabio Utzig <fabio@utzig.net>
2013-07-12 19:06:51 -0300

    Finish chapter 4 exercises

--- the_little_schemer/ch4/math.scm
+++ the_little_schemer/ch4/math.scm
@@ -54,3 +54,71 @@
       ((o> m n) #f)
       ((o< m n) #f)
       (else #t))))
+
+(define ex
+  (lambda (m n)
+    (cond
+      ((zero? n) 1)
+      (else (* m (ex m (sub1 n)))))))
+
+(define o/
+  (lambda (m n)
+    (cond
+      ((< m n) 0)
+      (else (add1 (o/ (- m n) n))))))
+
+(define len
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (len (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (n m)
+    (cond
+      ((and (number? n) (number? m)) (= n m))
+      (else (eq? n m)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      (else (cond
+        ((eq? a (car lat)) (add1 (occur a (cdr lat))))
+        (else (occur a (cdr lat))))))))
+
+(define one?
+  (lambda (n)
+    (= 1 n)))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
