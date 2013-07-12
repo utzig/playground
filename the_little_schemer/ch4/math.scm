10adcd6445ac14127a40b20badcf15ddb49221eb
Fabio Utzig <fabio@utzig.net>
2013-07-12 17:44:09 -0300

    More updates from ch4

--- the_little_schemer/ch4/math.scm
+++ the_little_schemer/ch4/math.scm
@@ -23,3 +23,34 @@
     (cond
       ((zero? m) 0)
       (else (+ n (x n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (+ (car tup1) (car tup2))
+        (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (m n)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else
+        (o> (sub1 m) (sub1 n))))))
+
+(define o<
+  (lambda (m n)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else
+        (o< (sub1 m) (sub1 n))))))
+
+(define o=
+  (lambda (m n)
+    (cond
+      ((o> m n) #f)
+      ((o< m n) #f)
+      (else #t))))
