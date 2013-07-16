2ba7b6060e78a906a5d97e1e504d8d086aecdbe2
Fabio Utzig <fabio@utzig.net>
2013-07-16 16:29:44 -0300

    Add some exercises from chapter 7

---
+++ the_little_schemer/ch7.scm
@@ -0,0 +1,46 @@
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+
+(define set?
+  (lambda (lat)
+    (cond
+      ((null? lat) #t)
+      ((member? (car lat) (cdr lat)) #f)
+      (else (set? (cdr lat))))))
+
+(define makeset
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((member? (car lat) (cdr lat))
+        (makeset (cdr lat)))
+      (else (cons (car lat) (makeset (cdr lat)))))))
+
+(define subset?
+  (lambda (set1 set2)
+      (cond
+        ((null? set1) #t)
+        (else (and (member? (car set1) set2) (subset? (cdr set1) set2))))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2) (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      (else (or (member? (car set1) set2) (intersect? (cdr set1) set2))))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      (else (cond
+        ((member? (car set1) set2) (cons (car set1)
+                                         (intersect (cdr set1) set2)))
+        (else (intersect (cdr set1) set2)))))))
