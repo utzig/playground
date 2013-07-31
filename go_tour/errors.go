4f4490a1652867777fcf012324f96606f36668fd
Fabio Utzig <fabio@utzig.net>
2013-07-31 12:52:35 -0300

    Go tour - errors

---
+++ go_tour/errors.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, ErrNegativeSqrt(f)
+	}
+	z := f
+	last := float64(0)
+	var i int
+	for i = 0; i < 10 && last != z; i++ {
+		last = z
+		z = z - ((z*z - f) / (2 * z))
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
