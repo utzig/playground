9cb801039c16217530d3bbf95bba640160a63831
Fabio Utzig <fabio@utzig.net>
2013-10-13 09:36:59 -0300

    Add chapter 8 challenge
    Date formatting with android.text.format.DateFormat class.

---
+++ android_programming_the_big_nerd_ranch_guide/chapter8/CriminalIntent-Challenges/src/utzig/net/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,68 @@
+package utzig.net.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mCrime = new Crime();
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup parent,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(
+					CharSequence c, int start, int before, int count) {
+				mCrime.setTitle(c.toString());
+			}
+			
+			public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+				// TODO
+			}
+			
+			public void afterTextChanged(Editable c) {
+				// TODO
+			}
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		String dateStr = DateFormat.format("EEEE, MMMM d, yyyy",
+				mCrime.getDate()).toString();
+		mDateButton.setText(dateStr);
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				mCrime.setSolved(isChecked);
+			}
+		});
+		
+		return v;
+	}
+
+}
