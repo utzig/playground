387309749f61cd2446f0250eea82b3f8e24ed022
Fabio Utzig <fabio@utzig.net>
2013-11-11 18:27:18 -0200

    Add chapter 9

---
+++ android_programming_the_big_nerd_ranch_guide/chapter9/CriminalIntent/src/utzig/net/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package utzig.net.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return new CrimeFragment();
+	}
+
+}
