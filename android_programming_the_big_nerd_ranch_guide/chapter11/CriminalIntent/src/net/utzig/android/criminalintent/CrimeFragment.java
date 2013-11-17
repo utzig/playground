a5d2148669cc45797720ae9ec2829d1e8c14380d
Fabio Utzig <fabio@utzig.net>
2013-11-16 22:14:00 -0200

    Add chapter 11

---
+++ android_programming_the_big_nerd_ranch_guide/chapter11/CriminalIntent/src/net/utzig/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,86 @@
+package net.utzig.android.criminalintent;
+
+import java.util.UUID;
+
+import utzig.net.android.criminalintent.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	
+	public static final String EXTRA_CRIME_ID =
+			"net.utzig.android.criminalintent.crime_id";
+	
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
+	
+	public static CrimeFragment newInstance(UUID crimeId) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+		
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		UUID crimeId = (UUID)getArguments()
+				.getSerializable(EXTRA_CRIME_ID);
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup parent,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			public void onTextChanged(
+					CharSequence c, int start, int before, int count) {
+				mCrime.setTitle(c.toString());
+			}
+			
+			public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+				// TODO
+			}
+			
+			public void afterTextChanged(Editable c) {
+				// TODO
+			}
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				mCrime.setSolved(isChecked);
+			}
+		});
+		
+		return v;
+	}
+
+}
