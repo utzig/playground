d0464052e5282cd6c392ae0a9517f5dbf8bb017e
Fabio Utzig <fabio@utzig.net>
2014-01-07 22:17:00 -0200

    Add chapter13

---
+++ android_programming_the_big_nerd_ranch_guide/chapter13/HelloMoon/src/AudioPlayer.java
@@ -0,0 +1,29 @@
+import net.utzig.android.hellomoon.R;
+import android.content.Context;
+import android.media.MediaPlayer;
+
+
+public class AudioPlayer {
+	
+	private MediaPlayer mPlayer;
+	
+	public void stop() {
+		if (mPlayer != null) {
+			mPlayer.release();
+			mPlayer = null;
+		}
+	}
+	
+	public void play(Context c) {
+		stop();
+		mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+		mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				stop();
+			}
+		});
+		mPlayer.start();
+	}
+
+}
