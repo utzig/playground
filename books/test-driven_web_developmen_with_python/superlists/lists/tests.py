50be3291ecc296c4a17aa0a08f1aa1024a925ce6
Fabio Utzig <utzig@utzig.org>
2014-12-02 09:52:47 -0200

    Finish chapter 3

--- books/test-driven_web_developmen_with_python/superlists/lists/tests.py
+++ books/test-driven_web_developmen_with_python/superlists/lists/tests.py
@@ -1,6 +1,18 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-class SmokeTest(TestCase):
+from lists.views import home_page
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
