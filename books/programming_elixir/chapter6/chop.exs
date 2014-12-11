4b56172b88332dcca8f9a304d315260a32da6afb
Fabio Utzig <utzig@utzig.org>
2014-12-11 09:19:35 -0200

    Programming Elixir ModulesAndFunctions-6

---
+++ books/programming_elixir/chapter6/chop.exs
@@ -0,0 +1,23 @@
+defmodule Chop do
+  defp current_guess(actual, range, value) when actual == value do
+    IO.puts "Is it #{value}"
+    IO.puts "#{actual}"
+  end
+
+  defp current_guess(actual, range, value) when value < actual do
+    IO.puts "Is it #{value}"
+    min..max = range
+    current_guess(actual, value..max, value + div(max - value, 2))
+  end
+
+  defp current_guess(actual, range, value) when value > actual do
+    IO.puts "Is it #{value}"
+    min..max = range
+    current_guess(actual, min..value, min + div(value - min, 2))
+  end
+
+  def guess(actual, range) do
+    min..max = range
+    current_guess(actual, range, min + div(max - min, 2))
+  end
+end
