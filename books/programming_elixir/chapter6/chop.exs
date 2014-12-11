1ee86cbab3fe6720121301c5644d24a373c1d217
Fabio Utzig <utzig@utzig.org>
2014-12-11 11:22:21 -0200

    Remove warning of unused variables

--- books/programming_elixir/chapter6/chop.exs
+++ books/programming_elixir/chapter6/chop.exs
@@ -1,18 +1,18 @@
 defmodule Chop do
-  defp current_guess(actual, range, value) when actual == value do
+  defp current_guess(actual, _range, value) when actual == value do
     IO.puts "Is it #{value}"
     IO.puts "#{actual}"
   end
 
   defp current_guess(actual, range, value) when value < actual do
     IO.puts "Is it #{value}"
-    min..max = range
+    _min..max = range
     current_guess(actual, value..max, value + div(max - value, 2))
   end
 
   defp current_guess(actual, range, value) when value > actual do
     IO.puts "Is it #{value}"
-    min..max = range
+    min.._max = range
     current_guess(actual, min..value, min + div(value - min, 2))
   end
 
