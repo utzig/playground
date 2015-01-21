599ae2754aeb97d93efa29f25559448d29171870
Fabio Utzig <utzig@utzig.org>
2015-01-21 20:59:59 -0200

    rename all item input ids and names. still broken

--- books/test-driven_web_developmen_with_python/functional_tests/test_layout_and_styling.py
+++ books/test-driven_web_developmen_with_python/functional_tests/test_layout_and_styling.py
@@ -7,7 +7,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
                 inputbox.location['x'] + inputbox.size['width'] / 2,
                 512,
@@ -15,7 +15,7 @@ class LayoutAndStylingTest(FunctionalTest):
         )
 
         inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
                 inputbox.location['x'] + inputbox.size['width'] / 2,
                 512,
