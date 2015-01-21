d5e45a5cf308e6db1f016533e15f52cefc033282
Fabio Utzig <utzig@utzig.org>
2015-01-21 21:46:13 -0200

    use form in all views + fix my own devsed errors

--- books/test-driven_web_developmen_with_python/functional_tests/test_list_item_validation.py
+++ books/test-driven_web_developmen_with_python/functional_tests/test_list_item_validation.py
@@ -12,9 +12,9 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
-        self.send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
 
-        self.get_item_input_box().check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
