c96c870adbca4478f1859c424cefe9dd1e9d311f
Fabio Utzig <utzig@utzig.org>
2015-11-11 15:16:25 -0200

    Finish chapter 13

--- books/test-driven_web_developmen_with_python/functional_tests/test_list_item_validation.py
+++ books/test-driven_web_developmen_with_python/functional_tests/test_list_item_validation.py
@@ -2,11 +2,14 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
 
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -15,7 +18,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
 
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Make tea\n')
@@ -30,5 +33,15 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
 
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        self.get_item_input_box().send_keys('a')
+
+        self.assertFalse(error.is_displayed())
