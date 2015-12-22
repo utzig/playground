19c01e64c6c4aa5adc2b526ed6d01814e3cd66c7
Fabio Utzig <utzig@utzig.org>
2015-12-22 18:34:56 -0200

    Add adventofcode day2

---
+++ adventofcode/day2.py
@@ -0,0 +1,16 @@
+with open("day2.txt") as f:
+    lines = f.readlines()
+
+total_area = 0
+total_ribbon = 0
+for line in lines:
+    l, w, h = map(int, line.split('x'))
+    side_areas = [l*w, w*h, h*l]
+    total_area += 2 * sum(side_areas) + min(side_areas)
+
+    total_ribbon += l * w * h
+    sides = [l, w, h]
+    sides.remove(max(sides))
+    total_ribbon += sum(sides) * 2
+
+print("Total area: %r, total ribbon: %r" % (total_area, total_ribbon,))
